921dbfd6cd564906b10180a6b288717944146485
erzh <cmpan@qq.com>
2017-07-21 10:25:12 +0800

    v 0.1

--- app/demo/controller/WelcomeController.php
+++ app/demo/controller/WelcomeController.php
@@ -2,7 +2,7 @@
 
 namespace app\demo\controller;
 
-class WelcomeController extends \wf\web\Controller {
+class WelcomeController extends \wf\app\web\Controller {
     
     public function indexAction() {
         $this->getView()
